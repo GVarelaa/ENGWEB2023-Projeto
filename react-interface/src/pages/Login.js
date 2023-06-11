a236bb2986aed6d562791afb4e846befb61d87e8
GVarelaa <guilherme.varelaaa@gmail.com>
2023-06-11 15:30:47 +0100

    pagina login feita

--- react-interface/src/pages/Login.js
+++ react-interface/src/pages/Login.js
@@ -1,5 +1,6 @@
 import { Link } from "react-router-dom";
 import { useState } from 'react';
+import { Col, Button, Row, Container, Card, Form, FloatingLabel } from "react-bootstrap";
 
 function Login() {
   const [name, setName] = useState("");
@@ -12,9 +13,51 @@ function Login() {
 
   return (
       <>
-        <header>
-          <h1>Login Form</h1>
-        </header>
+        <div style={{ background: 'linear-gradient(to right, rgba(250,244,49,1), rgba(237,204,6,1))' }}>
+          <Container>
+            <Row className="vh-100 d-flex align-items-center justify-content-center">
+              <Col md={8} lg={6} xs={12}>
+                <Card className="shadow-lg">
+                  <Card.Body>
+                    <div className="mb-3 mt-md-4">
+                      <h2 className="fw-bold mb-2">Acordãos</h2>
+                      <p className=" mb-5">Por favor insira os seus dados para iniciar sessão.</p>
+
+                      <Form onSubmit={handleSubmit}>
+                        <FloatingLabel className="mb-3 form-outline" label="Username">
+                          <Form.Control type="text" placeholder="Username"/>
+                        </FloatingLabel>
+
+                        <FloatingLabel  className="mb-3 form-outline" label="Password">
+                          <Form.Control type="password" placeholder="Password" />
+                        </FloatingLabel>
+                      </Form>
+
+                      <div className="d-flex justify-content-center">
+                        <Button type="submit" variant="outline-warning">Login</Button>
+                      </div>
+
+                      <div className="mt-3">
+                        <p className="mb-0  text-center">
+                          Não possui conta? {" "}
+                          <a href="/" className="text-warning fw-bold">
+                            Registar
+                          </a>
+                        </p>
+                      </div>
+                    </div>
+                  </Card.Body>
+                </Card>
+              </Col>
+            </Row>
+          </Container>
+        </div>
+      </>
+  );
+}
+
+/*
+        <div> className="form"</div>
 
         <form onSubmit={handleSubmit}>
           <fieldset>
@@ -25,8 +68,6 @@ function Login() {
             <input type="submit"/>
           </fieldset>
         </form>
-      </>
-  );
-}
+*/
 
 export default Login;
