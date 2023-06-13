5ec8cb68dc2a8be4d04e422654fdccf9b113812f
Guilherme Varela <guilherme.varelaaa@gmail.com>
2023-06-13 01:26:23 +0100

    falta paginaçao

--- API/app.js
+++ API/app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var cors = require('cors')
 
 var mongoose = require('mongoose');
 var mongoDB = 'mongodb://127.0.0.1/ProjetoEngWeb';
@@ -17,6 +18,7 @@ var indexRouter = require('./routes/index');
 
 var app = express();
 
+app.use(cors());
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
--- react-interface/src/pages/Home.js
+++ react-interface/src/pages/Home.js
@@ -1,40 +1,88 @@
+import { useState, useEffect} from 'react'
 import NavBar from '../components/NavBar';
-import { Table } from 'react-bootstrap';
+import { Container, Accordion, ListGroup, ListGroupItem, Pagination } from 'react-bootstrap';
 import axios from 'axios';
 var env = require('../config/env')
 
 
 function Home() {
-  var records;
-  var nItems = 50;
+  const [data, setData] = useState([]);
+  var nItems = 100;
 
-  axios.get(env.apiAcessPoint + '?skip=0&limit=' + nItems)
-    .then(response => {
-      records = response.data;
-    })
-    .catch(error => {
-      console.log(error)
-    })
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(env.apiAcessPoint + `?skip=0&limit=${nItems}`);
+        setData(response.data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
 
+    fetchData();
+  }, []);
+
+  console.log(data)
 
   return (
     <>
-      <NavBar />
-      <Table>
-        <thead>
-          <tr>
-            <td>Data Sessão</td>
-            <td>Nº Processo</td>
-            <td>Relator</td>
-          </tr>
-        </thead>
+      <NavBar/>
+      <Container>
+        <Accordion>
+          {data.map((obj, index) => {
+            return (
+              <Accordion.Item eventKey={index}>
+                <Accordion.Header><b>Processo:</b>{obj.Processo}</Accordion.Header>
+                <Accordion.Body>
+                  <ListGroup>
+                    <ListGroupItem><b>Data:</b>{obj.Data}</ListGroupItem>
+                    <ListGroupItem><b>Relator:</b>{obj.Relator}</ListGroupItem>
+                  </ListGroup>
+                </Accordion.Body>
+              </Accordion.Item>
+            )
+          })}
+        </Accordion>
+          <Pagination>
+            <Pagination.First />
+            <Pagination.Prev />
+            <Pagination.Item>{1}</Pagination.Item>
+            <Pagination.Ellipsis />
 
-        <tbody>
+            <Pagination.Item>{10}</Pagination.Item>
+            <Pagination.Item>{11}</Pagination.Item>
+            <Pagination.Item active>{12}</Pagination.Item>
+            <Pagination.Item>{13}</Pagination.Item>
+            <Pagination.Item disabled>{14}</Pagination.Item>
 
-        </tbody>
-      </Table>
+            <Pagination.Ellipsis />
+            <Pagination.Item>{20}</Pagination.Item>
+            <Pagination.Next />
+            <Pagination.Last />
+          </Pagination>
+      </Container>
     </>
   );
 }
+
+/*
+      <NavBar />
+      <Container>
+        <Table>
+          <thead>
+            <tr>
+              <th scope='col'>Data Sessão</th>
+              <th scope='col'>Nº Processo</th>
+              <th scope='col'>Relator</th>
+            </tr>
+          </thead>
+
+          <tbody>
+
+          </tbody>
+        </Table>
+      </Container>
+*/
+
 
 export default Home;
