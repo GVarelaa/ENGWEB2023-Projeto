e840461859d51d688b1a276be6e6177a85171e81
GVarelaa <guilherme.varelaaa@gmail.com>
2023-06-29 15:59:59 +0100

    descritores no meio e rotas protegidas

--- react-interface/src/components/RouterGuard.js
+++ react-interface/src/components/RouterGuard.js
@@ -2,20 +2,19 @@ import React from 'react';
 import { Navigate } from 'react-router-dom';
 import jwt_decode from 'jwt-decode'
 import Cookies from 'js-cookie'
+import NoPage from '../pages/NoPage';
 
 function RouterGuard({children, level}) {
- 
-    function hasJWT() {
-        let flag = false;
-        
+    function hasJWT() {        
         var token = localStorage.getItem('token')
         var decodedToken
 
         if(token){
             try{
                 decodedToken = jwt_decode(token)
-                if(Date.now() >= decodedToken.exp * 1000 && decodedToken.level >= level) flag = false
-                else flag = true
+                if(Date.now() >= decodedToken.exp * 1000) return 0
+                else if(decodedToken.level < level) return -1
+                else return 1
             }
             catch (error){}
         }
@@ -23,19 +22,21 @@ function RouterGuard({children, level}) {
             token = Cookies.get('token')
             try{
                 decodedToken = jwt_decode(token)
-                if(Date.now() >= decodedToken.exp * 1000 && decodedToken.level >= level) flag = false
+                if(Date.now() >= decodedToken.exp * 1000) return 0
+                else if(decodedToken.level < level) return -1
                 else {
                     localStorage.setItem('token', token)
-                    flag = true
+                    return 1
                 }
             }
             catch (error){}
         }
-       
-        return flag
+
+        return 0
     }
   
-    if(!hasJWT()) return <Navigate to="/login"/>;
+    if(hasJWT() === 0) return <Navigate to="/login"/>;
+    else if(hasJWT() === -1) return <NoPage/>; // Podemos melhorar
     else return children;
  };
 
