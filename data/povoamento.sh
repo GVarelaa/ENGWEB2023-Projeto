4ffb0f60fa1528608a2f748c319153eca4703168
gabrielasfc <gsfcunha@gmail.com>
2023-07-01 13:31:36 +0100

    script para povoar a db

---
+++ data/povoamento.sh
@@ -0,0 +1,4 @@
+mongoimport -d ProjetoEngWeb -c acordaos --file acordaos.json --jsonArray
+mongoimport -d ProjetoEngWeb -c tribunals --file tribunais.json --jsonArray
+mongoimport -d ProjetoEngWeb -c fields --file fields.json --jsonArray
+mongoimport -d ProjetoEngWeb -c acordaos_details --file acordaos_details.json --jsonArray
